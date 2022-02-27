13d77051d83a87056a7d30ff14d743e88be01774
greatvand <33458147+greatvand@users.noreply.github.com>
2022-02-27 12:14:46 +0200

    dummy commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
     public static void main (String args[]){
         System.out.println("Hello world from India and Syria!");
+        System.out.println("Hello world from Devops and Syria!");
     }
 }
